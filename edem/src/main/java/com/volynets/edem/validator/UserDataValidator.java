27f74da8d93eeaa43db2d22be035d1901b318352
volynetspavel <pavelsergeevich11@gmail.com>
2021-01-13 02:17:52 +0300

    created validator for user data

---
+++ edem/src/main/java/com/volynets/edem/validator/UserDataValidator.java
@@ -0,0 +1,68 @@
+package com.volynets.edem.validator;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Class validates user parameters.
+ * 
+ * @author Pavel Volynets
+ * @version 1.0
+ */
+public class UserDataValidator {
+	private Map<String, String> errorMessages = new HashMap<>();
+
+	private static final String STRING_REGEX_ALPHABETIC_STRING = "^(\\p{IsAlphabetic}+){1,50}$";
+	private static final String STRING_REGEX_EMAIL = "user11@gmail.com";		//"(\\w{6,})@(\\w+\\.)([a-z]{2,4})";
+	private static final String STRING_REGEX_PASSWORD = "user";		//"(?=.*[\\d])(?=.*[a-z])(?=.*[A-Z]).{7,16}";
+
+	public Map<String, String> validateData(String surname, String name, String avatar, String email, String password,
+			String confirmedPassword) {
+
+		errorMessages = validateData(surname, name, avatar, email);
+
+		Pattern regexPassword = Pattern.compile(STRING_REGEX_PASSWORD);
+
+		Matcher matcherPassword = regexPassword.matcher(password);
+		Matcher matcherConfirmedPassword = regexPassword.matcher(confirmedPassword);
+
+		if (!matcherPassword.matches() || !matcherConfirmedPassword.matches()) {
+			errorMessages.put("errorPassword", "Password does not match the requirements");
+		}
+
+		return errorMessages;
+	}
+
+	private Map<String, String> validateData(String surname, String name, String avatar, String email) {
+
+		Pattern regexAlphabeticString = Pattern.compile(STRING_REGEX_ALPHABETIC_STRING);
+		Pattern regexEmail = Pattern.compile(STRING_REGEX_EMAIL);
+
+		Matcher matcherSurname = regexAlphabeticString.matcher(surname);
+		Matcher matcherName = regexAlphabeticString.matcher(name);
+		Matcher matcherEmail = regexEmail.matcher(email);
+
+		if (!matcherSurname.matches()) {
+			errorMessages.put("errorSurname", "Surname can only use letters");
+		}
+
+		if (!matcherName.matches()) {
+			errorMessages.put("errorName", "Name can only use letters");
+		}
+
+		if (!matcherEmail.matches()) {
+			errorMessages.put("errorEmail", "Email does not match the requirements");
+		}
+
+		return errorMessages;
+	}
+
+	private boolean validatePass(String password) {
+		Pattern regexPassword = Pattern.compile(STRING_REGEX_PASSWORD);
+		Matcher matcherPassword = regexPassword.matcher(password);
+
+		return matcherPassword.matches();
+	}
+}
